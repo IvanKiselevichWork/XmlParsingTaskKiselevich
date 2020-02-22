9460b6eed1c9fd480f93205687bbd6980972311d
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-22 21:09:49 +0300

    Add ControllerServlet with Hello World output to /index url

---
+++ src/main/java/by/kiselevich/xmlparser/controller/ControllerServlet.java
@@ -0,0 +1,26 @@
+package by.kiselevich.xmlparser.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "ControllerServlet", urlPatterns = {"/index"})
+public class ControllerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + "Hello World!" + "</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
