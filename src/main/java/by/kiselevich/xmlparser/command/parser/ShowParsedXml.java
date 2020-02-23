005f9c6037c0ec083d0d147d7a11742f81633f6c
ivankiselevich <ivankiselevichwork@gmail.com>
2020-02-23 20:06:13 +0300

    Edit ShowParsedXml command to use XmlDomParser

--- src/main/java/by/kiselevich/xmlparser/command/parser/ShowParsedXml.java
+++ src/main/java/by/kiselevich/xmlparser/command/parser/ShowParsedXml.java
@@ -3,6 +3,7 @@ package by.kiselevich.xmlparser.command.parser;
 import by.kiselevich.xmlparser.command.Attribute;
 import by.kiselevich.xmlparser.command.Command;
 import by.kiselevich.xmlparser.service.upload.FileUploader;
+import by.kiselevich.xmlparser.service.xmlparser.dom.XmlDomParser;
 import by.kiselevich.xmlparser.view.Page;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,14 +22,24 @@ public class ShowParsedXml implements Command {
 
     @Override
     public Page execute(HttpServletRequest req, HttpServletResponse resp) {
-        req.setAttribute(Attribute.ERROR_MESSAGE.getValue(), "xml not parsed, WIP");
+
         List<File> fileList = fileUploader.uploadFiles(req);
         StringBuilder result = new StringBuilder();
-        result.append("Uploaded files:").append("<br>");
-        for(File file : fileList) {
-            result.append(file.getAbsolutePath()).append("<br>");
+        if (!fileList.isEmpty()) {
+            result.append("Uploaded files:").append("<br>");
+            for(File file : fileList) {
+                result.append(file.getAbsolutePath()).append("<br>");
+            }
+
+            XmlDomParser xmlDomParser = new XmlDomParser(fileList.get(0).getAbsolutePath());
+            String parsedXml = xmlDomParser.parse();
+
+            req.setAttribute(Attribute.PARSED_XML.getValue(), parsedXml);
+        } else {
+            result.append("No files uploaded").append("<br>");
         }
-        req.setAttribute(Attribute.PARSED_XML.getValue(), result.toString());
+
+        req.setAttribute(Attribute.ERROR_MESSAGE.getValue(), result.toString());
 
         return Page.SHOW_PARSED_XML;
     }
