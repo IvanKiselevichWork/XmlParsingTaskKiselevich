564ef1cb2d30e68bf4e2782608323af117272063
ivankiselevich <ivankiselevichwork@gmail.com>
2020-03-01 10:36:48 +0300

    Delete unused classes

--- src/main/java/by/kiselevich/xmlparser/specification/cookie/CookieSpecification.java
+++
@@ -1,8 +0,0 @@
-package by.kiselevich.xmlparser.specification.cookie;
-
-import by.kiselevich.xmlparser.entity.Cookie;
-import by.kiselevich.xmlparser.repository.cookie.CookieRepository;
-import by.kiselevich.xmlparser.specification.Specification;
-
-public interface CookieSpecification extends Specification<Cookie, CookieRepository> {
-}
